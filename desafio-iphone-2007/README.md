842f1f78488648a457c2ef1430958768e1f92d7d
wilsongenezini <wilsongenezini@hotmail.com>
2023-10-17 16:36:11 -0400

    ilustração de classes e interfaces em Java

---
+++ desafio-iphone-2007/README.md
@@ -0,0 +1,3 @@
+## Desafio de classes e interfaces em Java
+
+Projeto ilustrativo do curso de Java pela DIO, onde será abordado o conceito de classes e interfaces no formato de arquivos .java
---
+++ desafio-iphone-2007/src/Celular.java
@@ -0,0 +1,48 @@
+public class Celular implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música");    
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando música");;
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página");;
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba");;
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página");;
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando");;
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo");;
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz");;
+    }
+    
+}
